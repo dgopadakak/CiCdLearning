e3267e38cab539af23cf4bdcb72f7a5e65400903
dgopadakak <dgopadakakv3@gmail.com>
2024-03-14 21:15:18 +0300

    New test (doesn't work for now)

--- app/src/androidTest/java/com/dgopadakak/cicdlearning/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/dgopadakak/cicdlearning/ExampleInstrumentedTest.kt
@@ -1,12 +1,16 @@
 package com.dgopadakak.cicdlearning
 
-import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.replaceText
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 
-import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,10 +19,16 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
+    @Before
+    fun cleanEditText() {
+        onView(withId(R.id.edit_text_number)).perform(click())  // Нужно ли?
+        onView(withId(R.id.edit_text_number)).perform(replaceText(""))
+    }
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.dgopadakak.cicdlearning", appContext.packageName)
+    fun clickButtonHome() {
+        onView(withId(R.id.button_times_two)).perform(click())
+        onView(withId(R.id.text_view_result)).check(matches(withText(R.string.please_enter_a_number_text)))
     }
 }
